0c1b622e42a0dc243de24632002cd160f20a0d14
GabrielPolido <bielpolido@hotmail.com>
2021-09-22 22:44:08 -0300

    PWII

---
+++ resources/views/produtos/index.blade.php
@@ -0,0 +1,17 @@
+<h3>Produtos</h3>
+<a href="{{ route('produtos.create') }}">Novo</a>
+
+@if(count($produtos)>0)
+<ul>
+    @foreach ($produtos as $p)
+    <li>
+        <p>{{ $p['nome'] }} | {{ $p['qtd'] }} | R${{ $p['valor'] }}</p>
+        <a href="{{ route('produtos.show', $p['id']) }}">Detalhes</a>
+        <a href="{{ route('produtos.edit', $p['id']) }}">Alterar</a>
+    </li>
+    @endforeach
+
+</ul>
+@else
+    <h3>Não existem produtos cadastrados</h3>
+@endif
